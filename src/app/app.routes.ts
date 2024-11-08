056640d769c47dc733792fa7275a32c5149ae8ef
Luis Godoy <basketcase.net84@gmail.com>
2024-11-08 10:55:28 -0600

    ready structure optimized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'',    redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadChildren: () => import('./modules').then(m=>m.HomeModule) }
+
+];
